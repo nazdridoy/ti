fbc05fb813db7806d1ca8afdb2cb3c5cee1c82e2
Nazmul Hossain <nazmulridoy399@gmail.com>
2020-09-23 01:17:23 +0600

    added my repo

--- vuejs/src/App.vue
+++ vuejs/src/App.vue
@@ -15,7 +15,7 @@ export default {
   },
   data: function() {
     return {
-      github: "https://github.com/tks18/gindex-v4",
+      github: "https://github.com/nazdridoy/ti",
       netflix_black: false,
     };
   },
